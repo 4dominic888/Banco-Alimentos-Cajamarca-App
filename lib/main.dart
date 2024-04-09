93ebef64047ce22de589ce82e823ed55f99f678e
4dominic888 <4dominin888@gmail.com>
2024-04-08 20:07:44 -0500

    agregado de idioma español

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bancalcaj_app/modules/control_de_entrada/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
     runApp(const MainApp());
@@ -10,11 +12,17 @@ class MainApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return const MaterialApp(
-            home: Scaffold(
-                body: Center(
-                child: Text('Hello World!'),
-                ),
-            ),
+          title: "Banco de alimentos app",
+          locale: Locale('es', 'ES'),
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate
+          ],
+          supportedLocales: [
+            Locale('es', 'ES')
+          ],
+          home: ControlEntradaScreen()
         );
     }
 }
