5acb0386595c5ef60a5173ec90e6e0f3c135dd12
4dominic888 <4dominin888@gmail.com>
2024-04-10 11:41:27 -0500

    cambio de atributos, prototipo para pasar a mongodb

--- lib/modules/control_de_entrada/classes/entrada.dart
+++ lib/modules/control_de_entrada/classes/entrada.dart
@@ -1,32 +1,55 @@
-import 'package:bancalcaj_app/modules/control_de_entrada/classes/almacenero.dart';
+import 'package:bancalcaj_app/modules/control_de_entrada/classes/producto.dart';
 
 class Entrada {
+    int _id;
     DateTime _fecha;
     double _cantidad;
     String _proveedor;
-    List<String> _tipoProducto;
+    final List<Producto> _listaProductos;
+    List<String> _tipoProductos;
     String? _comentario;
-    Almacenero _almacenero;
 
-    Entrada({DateTime? fecha, required double cantidad, required String proveedor, required List<String> tipoProducto, String? comentario, required Almacenero almacenero}) :
+    Entrada({
+      required int id,
+      DateTime? fecha,
+      required double cantidad,
+      required String proveedor,
+      required List<Producto> listaProductos,
+      required List<String> tipoProducto,
+      String? comentario
+    }) :
+        _id = id,
         _fecha = fecha ?? DateTime.now(),
         _cantidad = cantidad,
         _proveedor = proveedor,
-        _tipoProducto = tipoProducto,
-        _comentario = comentario ?? '',
-        _almacenero = almacenero;
+        _listaProductos = listaProductos,
+        _tipoProductos = tipoProducto,
+        _comentario = comentario ?? '';
 
+
+    int get id => _id;
     DateTime get fecha => _fecha;
     double get cantidad => _cantidad;
     String get proveedor => _proveedor;
-    List<String> get tipoProducto => _tipoProducto;
+    List<Producto> get listaProductos=> _listaProductos;
+    List<String> get tipoProductos => _tipoProductos;
     String? get comentario =>_comentario;
-    Almacenero get almacenero => _almacenero;
 
     set fecha(DateTime fecha) => _fecha = fecha;
     set cantidad(double cantidad) => _cantidad = cantidad;
     set proveedor(String proveedor) => _proveedor = proveedor;
-    set tipoProducto(List<String> tipoProducto) => _tipoProducto = tipoProducto;
+    set tipoProductos(List<String> tipoProducto) => _tipoProductos = tipoProducto;
     set comentario(String? comentario) => _comentario = comentario;
-    set almacenero(Almacenero almacenero) => _almacenero = almacenero;
+
+    Map<String, dynamic> toJson(){
+      return{
+        "_id" : _id,
+        "fecha" : _fecha,
+        "cantidad": _cantidad,
+        "proveedor": _proveedor,
+        "listaProductos": _listaProductos,
+        "tipoProductos": _tipoProductos,
+        "comentario": _comentario
+      };
+    }
 }
