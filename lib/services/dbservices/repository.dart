f04cef5cc3b3ce089a9ac4ccc72b891c32c1deac
4dominic888 <4dominin888@gmail.com>
2024-04-10 11:39:46 -0500

    agregado de repositorio, usando dicho patron de diseño para base de datos futuras a implementar

---
+++ lib/services/dbservices/repository.dart
@@ -0,0 +1,7 @@
+abstract class Repository<T>{
+    Future add(T item);
+    Future<T?> getById(int id);
+    Future<List<T>> getAll();
+    Future update(int id, T item);
+    Future delete(int id);
+}
