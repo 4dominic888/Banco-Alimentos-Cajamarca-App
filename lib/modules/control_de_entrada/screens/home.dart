de95a150e612bec64c30e383e77ff7fa576b6ab1
4dominic888 <4dominin888@gmail.com>
2024-04-12 15:39:31 -0500

    actualizacion, modificacion de la interfaz y validadores

--- lib/modules/control_de_entrada/screens/home.dart
+++ lib/modules/control_de_entrada/screens/home.dart
@@ -1,13 +1,15 @@
-import 'package:bancalcaj_app/modules/control_de_entrada/classes/almacenero.dart';
 import 'package:bancalcaj_app/modules/control_de_entrada/classes/entrada.dart';
+import 'package:bancalcaj_app/modules/control_de_entrada/widgets/select_products.dart';
+
+import 'package:bancalcaj_app/modules/control_de_entrada/repositories/entrada_alimentos_repository.dart';
 import 'package:bancalcaj_app/modules/control_de_entrada/widgets/datetime_field.dart';
-import 'package:bancalcaj_app/modules/control_de_entrada/widgets/decimal_field.dart';
-import 'package:bancalcaj_app/modules/control_de_entrada/widgets/select_multiple_field.dart';
-import 'package:bancalcaj_app/modules/control_de_entrada/widgets/validate_text_field.dart';
+
+import 'package:bancalcaj_app/services/dbservices/data_base_service.dart';
 import 'package:flutter/material.dart';
 
 class ControlEntradaScreen extends StatefulWidget {
-  const ControlEntradaScreen({super.key});
+  const ControlEntradaScreen({super.key, required this.dbContext});
+  final DataBaseService dbContext;
 
   @override
   State<ControlEntradaScreen> createState() => _ControlEntradaScreenState();
@@ -18,16 +20,15 @@ class _ControlEntradaScreenState extends State<ControlEntradaScreen> {
   final formkey = GlobalKey<FormState>();
 
   final GlobalKey<DateTimeFieldState> _keyFieldFecha = GlobalKey();
-  final GlobalKey<DecimalFieldState> _keyFieldCantidad = GlobalKey();
-  final GlobalKey<SelectMultipleFieldState> _keyFieldProductos = GlobalKey();
-  final GlobalKey<ValidateTextFieldState> _keyFieldProveedor = GlobalKey();
+  final GlobalKey<FormFieldState> _keyFieldProveedor = GlobalKey();
+  final GlobalKey<SelectProductsFieldState> _keyFieldProductos = GlobalKey();
   final GlobalKey<FormFieldState> _keyFieldComentario = GlobalKey();
 
-  //TODO El almacenero deberia ser el usuario de la aplicacion.
-  final Almacenero almacenero = Almacenero(nombre: "nombre", DNI: "96541245", firma: "...");
+  late final EntradaAlimentosRepository entradaRepo;
+  bool _onLoad = false;
   
   //TODO: La lista deberia llamarse de alguna base de datos o similar, de momento esto sirve para testear.
-  List<String> defaultList = ["Carne", "Fruta", "Verdura", "Abarrote", "Embutido"];
+  List<String> defaultList = ["Carnes", "Frutas", "Verduras", "Abarrotes", "Embutidos"];
 
   Future _showAlert(BuildContext context, {required String title, required String content, required void Function() onPressed}){
     return showDialog(context: context, builder: (context) {
@@ -41,6 +42,63 @@ class _ControlEntradaScreenState extends State<ControlEntradaScreen> {
     });
   }
 
+  Future _onSubmit() async{
+
+    final fecha = _keyFieldFecha.currentState!.fecha;
+    final cantidad = _keyFieldProductos.currentState!.cantidadTotal;
+    final proveedor = _keyFieldProveedor.currentState!.value.toString();
+    final productos = _keyFieldProductos.currentState!.listProducts;
+    final comentario = _keyFieldComentario.currentState!.value.toString();
+
+    if(formkey.currentState!.validate()){
+      final Entrada p = Entrada(
+        fecha: fecha,
+        cantidad: cantidad,
+        proveedor: proveedor,
+        productos: productos,
+        comentario: comentario
+      );
+
+      //entradaRepo.add(p);
+      
+    }
+
+    /* //? codigo para validar que el dato ha sido registrado a la bd
+      entradaRepo.add(data).then(
+        (value){
+          _showAlert(
+            context,
+            title: "Exito",
+            content: "Se ha registrado la entrada de alimentos correctamente ${value.toString()}",
+            onPressed: () async {
+              Navigator.of(context).pop();
+              //TODO limpiar los campos
+            },
+          );
+        }
+      ).catchError((error){
+        if(error is http.ClientException){
+          _showAlert(context, title: "Error", content: "Compruebe su conexión a internet e inténtelo nuevamente", onPressed: ()=>Navigator.of(context).pop());
+        }
+      }).whenComplete(() {
+        setState(() {
+          _onLoad = false;
+        });
+      });
+      */
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    entradaRepo = EntradaAlimentosRepository(widget.dbContext);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
         return Scaffold(
@@ -50,104 +108,82 @@ class _ControlEntradaScreenState extends State<ControlEntradaScreen> {
             title: const Text("Entrada de alimentos"),
           ),
           body: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-            
-                  // Campo de fecha
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: DateTimeField(key: _keyFieldFecha, label: "Fecha")
-                  ),
-            
-            
-                  // Campo de cantidad en kg
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: DecimalField(key: _keyFieldCantidad, label: "Cantidad", suffixText: "Kg", emtpyValidate: "La cantidad no ha sido ingresada")
-                  ),
-            
-
-                  // Campo de proveedor o nombre de la organzación
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: ValidateTextField(
-                      key: _keyFieldProveedor,
-                      title: "Proveedor (Nombre de la organización)",
-                      emtpyValidate: "El proveedor no ha sido ingresado",
+            child: Form(
+              key: formkey,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+              
+                    // Campo de fecha
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: DateTimeField(key: _keyFieldFecha, label: "Fecha")
                     ),
-                  ),
-            
-                  // Campo de tipo de producto
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: SelectMultipleField(
-                      key: _keyFieldProductos,
-                      label: "Tipo de productos",
-                      hintModalCustom: 'Ingrese el nomnbre del producto',
-                      hintSelectElements: 'Seleccione los productos',
-                      titleModalCustom: 'Agregar un producto extra',
-                      list: defaultList,
-                      emtpyValidate: "Se debe seleccionar al menos un tipo de producto",
-                    ),
-                  ),
-            
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: TextFormField(
-                      key: _keyFieldComentario,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      decoration: const InputDecoration(label: Text("Comentarios u observaciones a tener en cuenta")),
-                    ),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        StringBuffer errorText = StringBuffer();
-                        final fechaState = _keyFieldFecha.currentState!;
-                        final cantidadState = _keyFieldCantidad.currentState!;
-                        final proveedorState = _keyFieldProveedor.currentState!;
-                        final productosState = _keyFieldProductos.currentState!;
-                        final comentarioState = _keyFieldComentario.currentState!;
-
-                        errorText.write(cantidadState.validator);
-                        errorText.write(proveedorState.validator);
-                        errorText.write(productosState.validator);
-
-                        if(errorText.isNotEmpty) { //* Si hay al menos un error
-                          _showAlert(
-                            context,
-                            title: "Ha ocurrido un error",
-                            content: errorText.toString(),
-                            onPressed: () => Navigator.of(context).pop(),
-                          );
-                        }
-                        else{
-                          _showAlert(
-                            context,
-                            title: "Exito",
-                            content: "Se ha registrado la entrada de alimentos correctamente",
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                              // api a excel drive
-                              //TODO limpiar los campos
-                            },
-                          );
-                        }
-
-
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.white
+              
+              
+                    // Campo de proveedor o nombre de la organzación
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: TextFormField(
+                        key: _keyFieldProveedor,
+                        validator: (value) {
+                          if(value!.trim().isEmpty) return "El proveedor no ha sido ingresado";
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                          labelText: "Proveedor",
+                          prefixIcon: Icon(Icons.person)
+                        ),
                       ),
-                      child: const Text("Registrar"),
                     ),
-                  ),                  
-                ],
+              
+                    // Campo de selección de productos
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SelectProductsField(
+                        key: _keyFieldProductos,
+                        defaultCommonProducts: defaultList,
+                      )
+                    ),
+              
+                    // Comentarios
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: TextFormField(
+                        key: _keyFieldComentario,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        decoration: const InputDecoration(
+                          label: Text("Comentarios u observaciones a tener en cuenta"),
+                          prefixIcon: Icon(Icons.comment)
+                        ),
+                      ),
+                    ),
+              
+                    // Submit button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                      child: Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: _onSubmit,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              foregroundColor: Colors.white
+                            ),
+                            child: const Text("Registrar"),
+                          ),
+                          const SizedBox(width: 20),
+                          SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: _onLoad ? const CircularProgressIndicator() : const SizedBox.shrink()
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+              ),
             ),
           )
         );
