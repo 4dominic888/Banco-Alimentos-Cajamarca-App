8ebde927100205a6695619ddaab5389965916bcd
4dominic888 <4dominin888@gmail.com>
2024-04-12 15:36:45 -0500

    implementacion de base de datos (aun no se usa)

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
 import 'package:bancalcaj_app/modules/control_de_entrada/screens/home.dart';
+import 'package:bancalcaj_app/services/dbservices/data_base_service.dart';
+import 'package:bancalcaj_app/services/dbservices/mongodb_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-    runApp(const MainApp());
+Future main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final dbContext = DataBaseService.getInstance(MongoDBService());
+    await dbContext.init();
+
+    runApp(MainApp(dbContext: dbContext));
 }
 
 class MainApp extends StatelessWidget {
-    const MainApp({super.key});
+    const MainApp({super.key, required this.dbContext});
+    final DataBaseService dbContext;
 
     @override
     Widget build(BuildContext context) {
-        return const MaterialApp(
+        return MaterialApp(
           title: "Banco de alimentos app",
-          locale: Locale('es', 'ES'),
-          localizationsDelegates: [
+          locale: const Locale('es', 'ES'),
+          localizationsDelegates: const [
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate
           ],
-          supportedLocales: [
+          supportedLocales: const [
             Locale('es', 'ES')
           ],
-          home: ControlEntradaScreen()
+          home: ControlEntradaScreen(dbContext: dbContext)
         );
     }
 }
