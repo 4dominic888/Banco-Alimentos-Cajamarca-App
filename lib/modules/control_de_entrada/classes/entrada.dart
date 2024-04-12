09678257fa267156c738a9dc2e57cb8213cc4968
4dominic888 <4dominin888@gmail.com>
2024-04-12 15:37:20 -0500

    formato de entrada para json (falta agregar almacenero)

--- lib/modules/control_de_entrada/classes/entrada.dart
+++ lib/modules/control_de_entrada/classes/entrada.dart
@@ -1,54 +1,43 @@
 import 'package:bancalcaj_app/modules/control_de_entrada/classes/producto.dart';
 
 class Entrada {
-    int _id;
     DateTime _fecha;
     double _cantidad;
     String _proveedor;
-    final List<Producto> _listaProductos;
-    List<String> _tipoProductos;
+    Map<String, List<Producto>> _productos;
     String? _comentario;
 
     Entrada({
-      required int id,
       DateTime? fecha,
       required double cantidad,
       required String proveedor,
-      required List<Producto> listaProductos,
-      required List<String> tipoProducto,
+      required Map<String, List<Producto>> productos,
       String? comentario
     }) :
-        _id = id,
         _fecha = fecha ?? DateTime.now(),
         _cantidad = cantidad,
         _proveedor = proveedor,
-        _listaProductos = listaProductos,
-        _tipoProductos = tipoProducto,
+        _productos = productos,
         _comentario = comentario ?? '';
 
 
-    int get id => _id;
     DateTime get fecha => _fecha;
     double get cantidad => _cantidad;
     String get proveedor => _proveedor;
-    List<Producto> get listaProductos=> _listaProductos;
-    List<String> get tipoProductos => _tipoProductos;
+    Map<String, List<Producto>> get productos => _productos;
     String? get comentario =>_comentario;
 
     set fecha(DateTime fecha) => _fecha = fecha;
     set cantidad(double cantidad) => _cantidad = cantidad;
     set proveedor(String proveedor) => _proveedor = proveedor;
-    set tipoProductos(List<String> tipoProducto) => _tipoProductos = tipoProducto;
     set comentario(String? comentario) => _comentario = comentario;
 
     Map<String, dynamic> toJson(){
       return{
-        "_id" : _id,
         "fecha" : _fecha,
         "cantidad": _cantidad,
         "proveedor": _proveedor,
-        "listaProductos": _listaProductos,
-        "tipoProductos": _tipoProductos,
+        "productos": _productos,
         "comentario": _comentario
       };
     }
