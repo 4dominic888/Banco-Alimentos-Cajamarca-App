6be9bec5b19c2d6cb134cc91fe23ae3cae98b604
4dominic888 <4dominin888@gmail.com>
2024-04-20 21:37:22 -0500

    cambio de dato para getAll

--- lib/services/dbservices/data_base_service.dart
+++ lib/services/dbservices/data_base_service.dart
@@ -9,7 +9,7 @@ abstract class DataBaseService{
   Future init();
   Future add(Map<String, dynamic> data);
   Future insert(int id, Map<String, dynamic> data);
-  Future<List<Map<String, dynamic>>> getAll();
+  Future<List<dynamic>> getAll();
   Future<Map<String, dynamic>> getById(int id);
   Future update(int id, Map<String, dynamic> newData);
   Future delete(int id);
