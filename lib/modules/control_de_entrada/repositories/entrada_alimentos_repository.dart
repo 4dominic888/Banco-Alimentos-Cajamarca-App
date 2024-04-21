518f8a2b996f1053aa0c7005f99acdb851401a85
4dominic888 <4dominin888@gmail.com>
2024-04-20 21:37:08 -0500

    cambio temporal para la obtención de entradas, json server

--- lib/modules/control_de_entrada/repositories/entrada_alimentos_repository.dart
+++ lib/modules/control_de_entrada/repositories/entrada_alimentos_repository.dart
@@ -1,6 +1,9 @@
+import 'package:bancalcaj_app/modules/control_de_entrada/classes/almacenero.dart';
 import 'package:bancalcaj_app/modules/control_de_entrada/classes/entrada.dart';
+import 'package:bancalcaj_app/modules/control_de_entrada/classes/producto.dart';
 import 'package:bancalcaj_app/services/dbservices/data_base_service.dart';
 import 'package:bancalcaj_app/services/dbservices/repository.dart';
+import 'package:intl/intl.dart';
 
 class EntradaAlimentosRepository implements Repository<Entrada> {
   
@@ -20,9 +23,34 @@ class EntradaAlimentosRepository implements Repository<Entrada> {
   }
 
   @override
-  Future<List<Entrada>> getAll() {
-    // TODO: implement getAll
-    throw UnimplementedError();
+  Future<List<Entrada>> getAll() async {
+    final json = await _context.getAll();
+    final List<Entrada> list = [];
+
+    for (var e in json) {
+
+      final Map<String, List<Producto>> productos = {};
+
+      final jsonProducts = e["productos"] as Map<String, dynamic>;
+
+      jsonProducts.forEach((key, value) {
+        final List<Producto> listaTipoProductos = [];
+        for (var rawProducts in value) {
+          listaTipoProductos.add(Producto(grupoAliementos: rawProducts["grupoAlimento"], peso: double.parse(rawProducts["peso"])));
+        }
+        productos[key] = listaTipoProductos;
+      });
+
+      list.add(Entrada(
+        fecha: DateFormat("d/M/y").parse(e["fecha"]),
+        cantidad: double.parse(e["cantidad"]),
+        proveedor: e["proveedor"],
+        productos: productos,
+        comentario: e["comentario"],
+        almacenero: Almacenero(nombre: e["almacenero"]["nombre"], dni: e["almacenero"]["dni"])
+      ));
+    }
+    return list;
   }
 
   @override
