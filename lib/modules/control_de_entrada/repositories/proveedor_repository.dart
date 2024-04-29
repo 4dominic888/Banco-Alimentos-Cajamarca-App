d686cfdcafaa17085f61c22fac5f67fac792a4da
4dominic888 <4dominin888@gmail.com>
2024-04-29 09:14:59 -0500

    reestructuracion de clases repositorio

--- lib/modules/control_de_entrada/repositories/proveedor_repository.dart
+++ lib/modules/control_de_entrada/repositories/proveedor_repository.dart
@@ -2,13 +2,11 @@ import 'package:bancalcaj_app/modules/control_de_entrada/classes/proveedor.dart'
 import 'package:bancalcaj_app/services/dbservices/data_base_service.dart';
 import 'package:bancalcaj_app/services/dbservices/repository.dart';
 
-class ProveedorRepository implements Repository<Proveedor>{
+class ProveedorRepository extends Repository<Proveedor>{
 
   final DataBaseService _context;
 
-  ProveedorRepository(DataBaseService context): _context = context{
-    _context.table = "proveedor";
-  }
+  ProveedorRepository(DataBaseService context): _context = context, super('proveedores');
 
   @override
   Future add(Proveedor item) {
@@ -23,10 +21,9 @@ class ProveedorRepository implements Repository<Proveedor>{
   }
 
   @override
-  Future<List<Proveedor>>? getAll() async {
-    final json = await _context.getAll();
-    if(json == null) return [];
-    return json.map((e) => Proveedor(id: e["idp"], nombre: e["nombre"])).toList();
+  Future<Iterable<Proveedor>?> getAll() async {
+    final json = (await _context.getAll(table))?.toList() ?? [];
+    return json.map((e) => Proveedor.fromJson(e));
   }
 
   @override
--- lib/services/dbservices/repository.dart
+++ lib/services/dbservices/repository.dart
@@ -1,7 +1,11 @@
 abstract class Repository<T>{
+    final String table;
+
+    Repository(this.table);
+
     Future add(T item);
     Future<T?> getById(int id);
-    Future<List<T>>? getAll();
+    Future<Iterable<T>?> getAll();
     Future update(int id, T item);
     Future delete(int id);
 }
