4ef429086fe4f4ed5f326536c12c7fab0b2fb892
4dominic888 <4dominin888@gmail.com>
2024-05-08 14:53:39 -0500

    soporte de file picker, pero para linux

--- lib/services/file_services/save_dialog.dart
+++ lib/services/file_services/save_dialog.dart
@@ -1,37 +1,67 @@
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
+import 'package:file_selector/file_selector.dart';
 import 'package:path_provider/path_provider.dart';
+
+Map<String, String> mapMimeType = {
+  'pdf': 'application/pdf',
+  'xlsx': 'application/vnd.ms-excel'
+};
 
 class SaveDialog{
 
-
-
   static Future<bool> onDownloadDir(List<int> bytes, {required String dialogTitle, required String filename, required String ext}) async{
     final Directory? downloadDir;
+    bool isFilePickerPackage;
     if(Platform.isAndroid){
       downloadDir = await getExternalStorageDirectory() ;
+      isFilePickerPackage = true;
     }
     else if(Platform.isWindows){
       downloadDir = await getDownloadsDirectory();
+      isFilePickerPackage = true;
+    }
+    else if(Platform.isLinux){
+      downloadDir = await getDownloadsDirectory();
+      isFilePickerPackage = false;
     }
     else{
       downloadDir = Directory('/');
+      isFilePickerPackage = true;
     }
 
-    final String? result = await FilePicker.platform.saveFile(
-      dialogTitle: dialogTitle,
-      fileName: '$filename.$ext',
-      type: FileType.custom,
-      allowedExtensions: [ext],
-      initialDirectory: downloadDir!.path,
-    );
+    if(isFilePickerPackage){
+      final String? result = await FilePicker.platform.saveFile(
+        lockParentWindow: true,
+        dialogTitle: dialogTitle,
+        fileName: '${filename.replaceAll(' ', '-')}.$ext',
+        type: FileType.custom,
+        allowedExtensions: [ext],
+        initialDirectory: downloadDir!.path,
+      );
 
-    if(result != null){
-      final File file = File(result);
-      file.writeAsBytes(bytes);
-      return true;
+      if(result != null){
+        final File file = File(result);
+        file.writeAsBytes(bytes);
+        return true;
+      }
+      return false;
     }
-    return false;
+    else{
+      final FileSaveLocation? result = await getSaveLocation(
+        suggestedName: '${filename.replaceAll(' ', '-')}.$ext',
+        initialDirectory: downloadDir!.path,
+        confirmButtonText: 'Guardar'
+      );
+
+      if(result != null){
+        final Uint8List fileData = Uint8List.fromList(bytes);
+        final XFile file = XFile.fromData(fileData, name: '${filename.replaceAll(' ', '-')}.$ext', mimeType: mapMimeType[ext]);
+        await file.saveTo(result.path);
+        return true;
+      }
+      return false;
+    }
   }
 }
