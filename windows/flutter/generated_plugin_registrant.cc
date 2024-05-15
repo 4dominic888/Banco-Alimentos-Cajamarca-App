a66f0720d83ee217356b50fdc534d9803b9deb46
4dominic888 <4dominin888@gmail.com>
2024-05-14 22:00:31 -0500

    esta cosa sigue actualizandose al parecer

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -5,6 +5,7 @@
 // clang-format off
 
 #include "generated_plugin_registrant.h"
+
 #include <file_selector_windows/file_selector_windows.h>
 #include <flutter_localization/flutter_localization_plugin_c_api.h>
 
