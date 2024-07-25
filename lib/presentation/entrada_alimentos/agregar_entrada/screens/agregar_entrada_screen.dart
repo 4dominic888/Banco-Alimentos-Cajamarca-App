49da321d3b417c902e9ead889b441e485476283d
4dominic888 <92335293+4dominic888@users.noreply.github.com>
2024-07-25 12:04:04 -0500

    Tmp enhancing UI (#21)
    * connectivity plus package removido
    
    * actualizacion de paquetes
    
    * jsonEntry para guardar referencia de proveedores y almaceneros
    
    * aplicacion del jsonEntry
    
    * reestructurado de validador de proveedor
    
    * dato de proveedor en la interfaz
    
    * file selector pacakge agregado y con sus cambios
    
    * soporte de file picker, pero para linux
    
    * no se que hice aca
    
    * arreglo de errores de parseo
    
    * cosa autogenerada
    
    * cambio de ruta de los repositorios
    
    * cambio de ruta para save dialog a util
    
    * cambio de nombre de la carpeta de db_services
    
    * cambio de ruta de repositorio, codigo mas ordenado
    
    * nombre de archivos de tipo screen cambiados para una mejor busqueda
    
    * agregando restricciones para valores minimos
    
    * implementando la edicion de las entradas
    
    * controlando relaciones nulas
    
    * cambiando posicion del boton de agregar proveedores a una parte mas comoda
    
    * agregando boton de acceso rapido a la vista de entradas
    
    * acelerando la vista de agregar proveedores
    
    * zoom improvisado
    
    * cambiando aspecto de las opciones
    
    * se puede regresar con la tecla escape

--- lib/presentation/entrada_alimentos/agregar_entrada/screens/agregar_entrada_screen.dart
+++ lib/presentation/entrada_alimentos/agregar_entrada/screens/agregar_entrada_screen.dart
@@ -12,6 +12,7 @@ import 'package:bancalcaj_app/presentation/widgets/loading_process_button.dart';
 import 'package:bancalcaj_app/presentation/widgets/notification_message.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rounded_loading_button_plus/rounded_loading_button.dart';
 
@@ -92,101 +93,107 @@ class _AgregarEntradaScreenState extends State<AgregarEntradaScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white,
-        title: const Text("Entrada de alimentos"),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
+    return CallbackShortcuts(
+      bindings: { const SingleActivator(LogicalKeyboardKey.escape) : Navigator.of(context).pop },
+      child: Focus(
+        autofocus: true,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            title: const Text("Entrada de alimentos"),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          body: FutureBuilder<Result<Entrada?>>(
+            future: entradaService.seleccionarEntrada(widget.idEntradaToEdit ?? 'null'),
+            builder: (context, snapEntrada) {
+        
+              if(snapEntrada.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+        
+              final entrada = snapEntrada.data!.data;
+              _isUpdatable = entrada != null;
+        
+              return SingleChildScrollView(
+                child: Form(
+                  key: formkey,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Campo de fecha
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: DateTimeField(
+                            initialValue: entrada?.fecha,
+                            formFieldKey: _keyFieldFecha,
+                            label: "Fecha"
+                          )
+                        ),
+                  
+                        // Campo de proveedor o nombre de la organzación
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: DropDownWithExternalData<ProveedorView>(
+                            initialValue: entrada?.proveedor?.proveedorView,
+                            formFieldKey: _keyFieldProveedor,
+                            itemAsString: (value) => value.nombre,
+                            label: 'Proveedor',
+                            icon: const Icon(Icons.delivery_dining),
+                            asyncItems: (text) async {
+                              final result = await proveedorService.verProveedores(pagina: 1, limite: 8, nombre: text);
+                              if(!result.success || result.data == null) return [];
+                              return result.data!.data;
+                            },
+                          )
+                        ),
+                  
+                        // Campo de selección de productos
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: SelectProductsField(
+                            initialValue: entrada?.tiposProductos,
+                            formFieldKey: _keyFieldProductos,
+                            defaultCommonProducts: defaultList,
+                          )
+                        ),
+                  
+                        // Comentarios
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: TextFormField(
+                            initialValue: entrada?.comentario,
+                            key: _keyFieldComentario,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            decoration: const InputDecoration(
+                              label: Text("Comentarios u observaciones a tener en cuenta"),
+                              prefixIcon: Icon(Icons.comment)
+                            ),
+                          ),
+                        ),
+              
+                        // Submit button
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                          child: LoadingProcessButton(
+                            controller: _btnController,
+                            label: Text(_isUpdatable ? 'Actualizar' : 'Registrar', style: const TextStyle(color: Colors.white)),
+                            color: _isUpdatable ? Colors.blue : Colors.red,
+                            proccess: _onSubmit,
+                          )
+                        ),
+                      ],
+                  ),
+                ),
+              );
+            }
+          )
         ),
       ),
-      body: FutureBuilder<Result<Entrada?>>(
-        future: entradaService.seleccionarEntrada(widget.idEntradaToEdit ?? 'null'),
-        builder: (context, snapEntrada) {
-
-          if(snapEntrada.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          final entrada = snapEntrada.data!.data;
-          _isUpdatable = entrada != null;
-
-          return SingleChildScrollView(
-            child: Form(
-              key: formkey,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Campo de fecha
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: DateTimeField(
-                        initialValue: entrada?.fecha,
-                        formFieldKey: _keyFieldFecha,
-                        label: "Fecha"
-                      )
-                    ),
-              
-                    // Campo de proveedor o nombre de la organzación
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: DropDownWithExternalData<ProveedorView>(
-                        initialValue: entrada?.proveedor?.proveedorView,
-                        formFieldKey: _keyFieldProveedor,
-                        itemAsString: (value) => value.nombre,
-                        label: 'Proveedor',
-                        icon: const Icon(Icons.delivery_dining),
-                        asyncItems: (text) async {
-                          final result = await proveedorService.verProveedores(pagina: 1, limite: 8, nombre: text);
-                          if(!result.success || result.data == null) return [];
-                          return result.data!.data;
-                        },
-                      )
-                    ),
-              
-                    // Campo de selección de productos
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: SelectProductsField(
-                        initialValue: entrada?.tiposProductos,
-                        formFieldKey: _keyFieldProductos,
-                        defaultCommonProducts: defaultList,
-                      )
-                    ),
-              
-                    // Comentarios
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: TextFormField(
-                        initialValue: entrada?.comentario,
-                        key: _keyFieldComentario,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        decoration: const InputDecoration(
-                          label: Text("Comentarios u observaciones a tener en cuenta"),
-                          prefixIcon: Icon(Icons.comment)
-                        ),
-                      ),
-                    ),
-          
-                    // Submit button
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                      child: LoadingProcessButton(
-                        controller: _btnController,
-                        label: Text(_isUpdatable ? 'Actualizar' : 'Registrar', style: const TextStyle(color: Colors.white)),
-                        color: _isUpdatable ? Colors.blue : Colors.red,
-                        proccess: _onSubmit,
-                      )
-                    ),
-                  ],
-              ),
-            ),
-          );
-        }
-      )
     );
   }
 }
