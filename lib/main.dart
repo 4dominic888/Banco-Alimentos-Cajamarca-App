e50d43244d4e763cfa8fe4eba036b0da869a0c91
4dominic888 <4dominin888@gmail.com>
2024-07-30 21:22:34 -0500

    colocando interfaz de autenticacion, solo esta login y logout funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'dart:async';
+import 'package:animated_floating_buttons/widgets/animated_floating_action_button.dart';
+import 'package:bancalcaj_app/domain/services/employee_service_base.dart';
+import 'package:bancalcaj_app/infrastructure/auth_utils.dart';
 import 'package:bancalcaj_app/locator.dart';
+import 'package:bancalcaj_app/presentation/empleados/login/screens/login_empleado_screen.dart';
 import 'package:bancalcaj_app/presentation/entrada_alimentos/ver_entradas/screens/ver_entradas_screen.dart';
 import 'package:bancalcaj_app/presentation/entrada_alimentos/agregar_entrada/screens/agregar_entrada_screen.dart';
 import 'package:bancalcaj_app/presentation/proveedores/agregar_proveedor/screens/agregar_proveedor_screen.dart';
 import 'package:bancalcaj_app/presentation/proveedores/ver_proveedores/screens/ver_proveedores_screen.dart';
+import 'package:bancalcaj_app/presentation/widgets/notification_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_it/get_it.dart';
 import 'package:overlay_support/overlay_support.dart';
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
+  
+  //* Get the instance of employee
+  await GetIt.I<EmployeeGeneralState>().refreshEmployee();
 
   runApp(const MainApp());
 }
@@ -41,37 +50,103 @@ class MainApp extends StatelessWidget {
 }
 
 //? Screen temporal, solo para testeo
-class _RouterScreen extends StatelessWidget {
+class _RouterScreen extends StatefulWidget {
   const _RouterScreen();
-  
+
+  @override
+  State<_RouterScreen> createState() => _RouterScreenState();
+}
+
+class _RouterScreenState extends State<_RouterScreen> {
+
+  List<Widget> unregisteredOptions() => [
+    FloatingActionButton(
+      heroTag: 'login',
+      tooltip: 'Iniciar sesion',
+      child: const Icon(Icons.login_sharp),
+      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const LoginEmpleadoScreen())).then((value) => setState(() {})),
+    ),
+    
+    const SizedBox.shrink()  
+  ];
+
+  List<Widget> employeeOptions() => [
+    FloatingActionButton(
+      heroTag: 'logout',
+      tooltip: 'Salir de la sesion',
+      child: const Icon(Icons.exit_to_app),
+      onPressed: () async {
+        final result = await GetIt.I<EmployeeServiceBase>().logout();
+        if(!result.success){
+          NotificationMessage.showErrorNotification(result.message);
+          return;
+        }
+        NotificationMessage.showSuccessNotification('Se ha cerrado sesion correctamente');
+        setState(() {});
+      },
+    ),
+
+    FloatingActionButton(
+      heroTag: 'editAccout',
+      tooltip: 'Edita tu cuenta',
+      child: const Icon(Icons.edit),
+      onPressed: (){
+        //TODO vista para editar datos
+      },
+    )
+  ];
+
+  List<Widget> administratorOptions() => [
+    FloatingActionButton(
+      heroTag: 'monitorate',
+      tooltip: 'Monitorea a los empleados',
+      child: const Icon(Icons.app_registration_rounded),
+      onPressed: () async {
+        //TODO Vista de monitorear usuarios
+      },
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(backgroundColor: Colors.red, foregroundColor: Colors.white, title: const Text("Exportar entrada")),
+        persistentFooterButtons: [
+          Text(GetIt.I<EmployeeGeneralState>().employee.nombre),
+          Text(GetIt.I<EmployeeGeneralState>().employee.dni),
+          // Text(AuthUtils.refreshToken ?? 'no token')
+        ],
+        floatingActionButton: AnimatedFloatingActionButton(          
+          fabButtons: AuthUtils.refreshToken == null ? unregisteredOptions() : employeeOptions()
+          ,
+          colorStartAnimation: Colors.red.shade300,
+          colorEndAnimation: Colors.red.shade200,
+          animatedIconData: AnimatedIcons.home_menu,
+        ),
         body: Padding(
-          padding: const EdgeInsets.all(32.0),
+          padding: const EdgeInsets.all(28.0),
           child: SizedBox(
-            width: MediaQuery.of(context).size.width,
+            width: MediaQuery.of(context).size.width / 1,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
+              mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Column(
                   children: [
                     const Text('Entradas de alimentos',
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)
                     ),
                     Expanded(
                       child: Wrap(
                         runSpacing: 15,
-                        spacing: 15,
+                        spacing: 5,
                         runAlignment: WrapAlignment.spaceBetween,
                         children: [
                           SizedBox(
                             width: 300,
                             child: Card(child: ListTile(
-                              title: const Text('Registrar Entradas de alimentos', style: TextStyle(fontSize: 25)),
+                              title: const Text('Registrar Entradas de alimentos', style: TextStyle(fontSize: 16)),
                               leading: const Icon(Icons.app_registration_rounded),
                               onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const AgregarEntradaScreen()))
                             ))
@@ -79,7 +154,7 @@ class _RouterScreen extends StatelessWidget {
                           SizedBox(
                             width: 260,
                             child: Card(child: ListTile(
-                              title: const Text('Ver Entradas de alimentos', style: TextStyle(fontSize: 25)),
+                              title: const Text('Ver Entradas de alimentos', style: TextStyle(fontSize: 16)),
                               leading: const Icon(Icons.list),
                               onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const VerEntradasScreen()))
                             ))
@@ -96,7 +171,7 @@ class _RouterScreen extends StatelessWidget {
                   child: Column(
                     children: [
                       const Text('Proveedores',
-                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)
                       ),
                       Wrap(
                         runSpacing: 15,
@@ -104,9 +179,9 @@ class _RouterScreen extends StatelessWidget {
                         runAlignment: WrapAlignment.spaceBetween,
                         children: [
                           SizedBox(
-                            width: 260,
+                            width: 280,
                             child: Card(child: ListTile(
-                              title: const Text('Registrar Proveedores', style: TextStyle(fontSize: 25)),
+                              title: const Text('Registrar Proveedores', style: TextStyle(fontSize: 16)),
                               leading: const Icon(Icons.person_add_alt),
                               onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const AgregarProveedorScreen()))
                             ))
@@ -114,7 +189,7 @@ class _RouterScreen extends StatelessWidget {
                           SizedBox(
                             width: 260,
                             child: Card(child: ListTile(
-                              title: const Text('Ver proveedores', style: TextStyle(fontSize: 25)),
+                              title: const Text('Ver proveedores', style: TextStyle(fontSize: 16)),
                               leading: const Icon(Icons.group),
                               onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const VerProveedoresScreen()))
                             ))
