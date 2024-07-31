4951722288a1e0b652189b2885adea2d90ba3a6e
4dominic888 <4dominin888@gmail.com>
2024-07-30 21:21:33 -0500

    arreglando headers y parseando correctamente el body

--- lib/data/backend/express_backend.dart
+++ lib/data/backend/express_backend.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dartx/dartx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -13,27 +14,32 @@ class ExpressBackend {
   static final prefs = GetIt.I<SharedPreferences>();
 
   static const Duration _timeLimit = Duration(seconds: 15);
-  static const header = { 'Content-Type': 'application/json' };
+  static const header = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  };
 
   static Map<String, String> _headers([bool needPermission = true]) {
+    Map<String, String> newHeader = header.toMap();
     final String? token = prefs.getString('token');
-
-    if(needPermission && token != null) { ({ ...header, 'Authorization': 'Bearer $token'} ); }
-    return header;
+    if(needPermission && token != null) { newHeader.addAll({'Authorization': 'Bearer $token'}); }
+    return newHeader;
   }
 
   static const String _domain = 'backend-test-bacalcaj.onrender.com';
+
+  static String? parseBody(Map<String, dynamic>? body) => body != null ? json.encode(body) : null;
 
   static Future<Map<String, dynamic>> solicitude(String route, RequestType requestType, {Map<String, dynamic>? queryParameters, Map<String, dynamic>? body, bool? needPermission = false}) async {
     final uri = Uri.https(_domain, 'api/$route', queryParameters);
     late final http.Response response;
 
     switch (requestType) {
-      case RequestType.get:    {  response = await http.get(    uri,                             headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
-      case RequestType.post:   {  response = await http.post(   uri,   body: json.encode(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
-      case RequestType.put:    {  response = await http.put(    uri,   body: json.encode(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
-      case RequestType.patch:  {  response = await http.patch(  uri,   body: json.encode(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
-      case RequestType.delete: {  response = await http.delete( uri,   body: json.encode(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
+      case RequestType.get:    {  response = await http.get(    uri,                           headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
+      case RequestType.post:   {  response = await http.post(   uri,   body: parseBody(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
+      case RequestType.put:    {  response = await http.put(    uri,   body: parseBody(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
+      case RequestType.patch:  {  response = await http.patch(  uri,   body: parseBody(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
+      case RequestType.delete: {  response = await http.delete( uri,   body: parseBody(body),  headers: _headers(needPermission!)).timeout(_timeLimit);   break; }
       default: throw ArgumentError('requestType no valido');
     }
 
