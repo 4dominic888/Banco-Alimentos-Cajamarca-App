068ba8f5ef1da7a8c5a15ec34b8156ffb3be97d3
4dominic888 <4dominin888@gmail.com>
2024-08-01 18:56:43 -0500

    implementando funcionalidad de edicion

--- lib/presentation/empleados/editar_cuenta/screens/editar_cuenta_screen.dart
+++ lib/presentation/empleados/editar_cuenta/screens/editar_cuenta_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bancalcaj_app/domain/models/employee.dart';
 import 'package:bancalcaj_app/domain/services/employee_service_base.dart';
 import 'package:bancalcaj_app/presentation/widgets/loading_process_button.dart';
+import 'package:bancalcaj_app/presentation/widgets/multi_drop_down_form_field.dart';
 import 'package:bancalcaj_app/presentation/widgets/notification_message.dart';
 import 'package:bancalcaj_app/presentation/widgets/password_form_field.dart';
 import 'package:flutter/material.dart';
@@ -11,8 +12,9 @@ import 'package:rounded_loading_button_plus/rounded_loading_button.dart';
 class EditarCuentaScreen extends StatefulWidget {
 
   final Employee employee;
+  final bool? superEdit;
 
-  const EditarCuentaScreen({super.key, required this.employee});
+  const EditarCuentaScreen({super.key, required this.employee, this.superEdit = false});
 
   @override
   State<EditarCuentaScreen> createState() => _EditarCuentaScreenState();
@@ -28,6 +30,95 @@ class _EditarCuentaScreenState extends State<EditarCuentaScreen> {
   final _passwordKey = GlobalKey<FormFieldState<String>>();
   final _verifyPasswordKey = GlobalKey<FormFieldState<String>>();
   final _btnPasswordController = RoundedLoadingButtonController();
+
+  final _rolesKey = GlobalKey<FormFieldState<List<EmployeeType>>>();
+  final _btnRolesController = RoundedLoadingButtonController();
+
+  Widget switcherDependingSuperEdit(){
+    //* Password stuffs
+    if(!widget.superEdit!){
+      return Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+              child: PasswordFormField(
+                formKey: _passwordKey,
+                label: 'Nueva Contraseña',
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) return 'Se debe proporcionar una contraseña';
+                  value = value.trim();
+                  if(value.length < 6) return 'Contraseña muy corta';
+                  return null;
+                },
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+              child: VerifyPasswordFormField(
+                formKey: _verifyPasswordKey,
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty) return 'Se debe proporcionar una contraseña';
+                  value = value.trim();
+                  if(value != _passwordKey.currentState!.value!) return 'las contraseñas no coinciden';
+                  return null;                          
+                },
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: LoadingProcessButton(
+                controller: _btnPasswordController,
+                label: const Text('Actualizar contraseña', style: TextStyle(color: Colors.white)),
+                color: Colors.red,
+                proccess: _updatePassword,
+              )
+            ),
+          )
+        ],
+      );
+    }
+    //* Rol stuffs
+    else{
+      return Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+              child: MultiDropDownFormField<EmployeeType>(
+                selectedItems: widget.employee.types,
+                formFieldKey: _rolesKey,
+                items: EmployeeType.values,
+                itemAsString: (value) => Employee.employeeTypeMap[value]!,
+                label: 'Roles del usuario',
+                compareFn: (v1, v2) => v1.index == v2.index,
+                icon: const Icon(Icons.type_specimen),                
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              child: LoadingProcessButton(
+                controller: _btnRolesController,
+                label: const Text('Actualizar roles', style: TextStyle(color: Colors.white)),
+                color: Colors.red,
+                proccess: _updateRoles,
+              )
+            ),
+          )
+
+        ],
+      );
+    }
+  }
 
   Future<void> _updateName() async {
     if(_nameKey.currentState!.validate()) {
@@ -63,6 +154,25 @@ class _EditarCuentaScreenState extends State<EditarCuentaScreen> {
       return;
     }
     _btnPasswordController.error();
+  }
+
+  Future<void> _updateRoles() async {
+    if(_rolesKey.currentState!.validate()) {
+
+      final roles = _rolesKey.currentState!.value!;
+
+      final result = await _employeeService.actualizarRoles(widget.employee.dni, roles);
+      if(!result.success){
+        _btnRolesController.error();
+        NotificationMessage.showErrorNotification(result.message);
+        return;
+      }
+
+      _btnRolesController.success();
+      NotificationMessage.showSuccessNotification('Actualizacion de roles exitoso');
+      return;
+    }
+    _btnRolesController.error();
   }
 
   @override
@@ -121,53 +231,8 @@ class _EditarCuentaScreenState extends State<EditarCuentaScreen> {
                 ],
               ),
 
-              //* Change password field
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
-                      child: PasswordFormField(
-                        formKey: _passwordKey,
-                        label: 'Nueva Contraseña',
-                        validator: (value) {
-                          if(value == null || value.trim().isEmpty) return 'Se debe proporcionar una contraseña';
-                          value = value.trim();
-                          if(value.length < 6) return 'Contraseña muy corta';
-                          return null;
-                        },
-                      ),
-                    ),
-                  ),
+              switcherDependingSuperEdit()
 
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
-                      child: VerifyPasswordFormField(
-                        formKey: _verifyPasswordKey,
-                        validator: (value) {
-                          if(value == null || value.trim().isEmpty) return 'Se debe proporcionar una contraseña';
-                          value = value.trim();
-                          if(value != _passwordKey.currentState!.value!) return 'las contraseñas no coinciden';
-                          return null;                          
-                        },
-                      ),
-                    ),
-                  ),
-
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                      child: LoadingProcessButton(
-                        controller: _btnPasswordController,
-                        label: const Text('Actualizar contraseña', style: TextStyle(color: Colors.white)),
-                        color: Colors.red,
-                        proccess: _updatePassword,
-                      )
-                    ),
-                  )
-                ],
-              )
             ],
           ),
         ),
